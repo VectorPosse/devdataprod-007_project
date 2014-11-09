364416d5c16e6561defbeb2db58b828e32397a19
VectorPosse <sean.raleigh@gmail.com>
2014-11-09 01:00:54 -0700

    Nice working version of Shiny app

--- ui.R
+++ ui.R
@@ -4,13 +4,21 @@ shinyUI(pageWithSidebar(
     headerPanel("Taylor Polynomials"),
     sidebarPanel(
         h3("Function and parameters"),
-        selectInput("f", "Function:",
+        selectInput("f", "Function: \\(f(x) = \\)",
                     choices = c("sin(x)" = "sin", "cos(x)" = "cos", "exp(x)" = "exp")),
-        sliderInput("n", "\\(n\\) (power of polynomial)",
-                    min = 0, max = 10, value = 1, step = 1)
+        sliderInput("n_pow", "\\(n\\) (power of polynomial):",
+                    value = 3, min = 1, max = 15),
+        sliderInput("x_min", "Lower bound for \\(x\\):",
+                    value = -5, min = -10, max = -1),
+        sliderInput("x_max", "Upper bound for \\(x\\):",
+                    value = 3, min = 1, max = 10)
     ),
     mainPanel(
-        withMathJax(h3("Plot of \\(f(x)\\) and \\(T(x)\\)")),
-        textOutput("text")
+        withMathJax("\\(\\require{color}\\)"),
+        withMathJax(h3("Plot of \\(\\textcolor{blue}{f(x)}\\) and its \\(n\\)th degree
+            Taylor polynomial \\(\\textcolor{red}{T_{n}(x)}\\)")),
+        uiOutput("input_f"),
+        uiOutput("text"),
+        plotOutput("plot")
     )
 ))
